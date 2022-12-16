45bb720e488d337c73c797e064e22e0261f89144
daniyal <daniyalmuhammad977@gmail.com>
2022-12-16 18:26:12 +0500

    first commit asda

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ console.log("i am server file")
 
 import express from 'express';
 const app = express()
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     console.log("request ip", req.ip);
