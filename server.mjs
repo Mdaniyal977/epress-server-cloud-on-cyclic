f73fa647249a0e05a4ae870e2ae37bfff5f896f3
daniyal <daniyalmuhammad977@gmail.com>
2022-12-17 19:35:41 +0500

    sada

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ console.log("i am server file")
 import express from 'express';
 import path from 'path';
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 
 app.get('/daniyal', (req, res) => {
