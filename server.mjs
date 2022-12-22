a1720464d843d158083c34f97e2b745c21c1887f
daniyal <daniyalmuhammad977@gmail.com>
2022-12-22 23:58:02 +0500

    worled

--- server.mjs
+++ server.mjs
@@ -6,20 +6,20 @@ const app = express()
 const port = process.env.PORT || 5001
 
 
-app.get('/', (req, res) => {
+app.get('/daniyal', (req, res) => {
     console.log("request ip", req.ip);
   res.send('Hello World here now!' + new Date().toString() );
 })
 
 
-app.get('/daniyal', (req, res) => {
+app.get('/dani', (req, res) => {
   console.log("request ip", req.ip);
 res.send('Hello World here now!' + new Date().toString() );
 })
 
 const __dirname = path.resolve();
 app.use('/',express.static(path.join(__dirname, './web/build')))
-app.get('/**',express.static(path.join(__dirname, './web/build/index.html')))
+app.use('/**',express.static(path.join(__dirname, './web/build/index.html')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
